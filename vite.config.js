8e1f6dd5f279a85a62d09c9bc0cb67006a9331b6
montaser-hub <60078290+montaser-hub@users.noreply.github.com>
2025-10-08 17:53:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ react() ],
-  base: '/materialui-news-explorer/',
+  base: 'cors-anywhere.herokuapp.com/https://newsapi.org/v2/',
 })
